92a720c7dfe669cf0dac12aff63299508d71f4de
Kilian Koeltzsch <me@kilian.io>
2017-05-20 13:51:02 +0200

    Initial Commit

---
+++ AbfahrtsTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AbfahrtsTV
+//
+//  Created by Kilian Költzsch on 20.05.17.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
